0feb5a53b312550baf70f0834b4a003bbd559217
gruan@asnum.com <gruan@asnum.com>
2016-04-28 15:17:48 +0800

    baidumap

--- AsNum.Control.Droid/WebBaiduMapRender.cs
+++ AsNum.Control.Droid/WebBaiduMapRender.cs
@@ -16,6 +16,9 @@ using Android.Graphics;
 using Xamarin.Forms;
 using AsNum.XFControls;
 using AsNum.XFControls.Droid;
+using Java.Interop;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 [assembly: ExportRenderer(typeof(WebBaiduMap), typeof(WebBaiduMapRender))]
 namespace AsNum.XFControls.Droid {
@@ -26,7 +29,7 @@ namespace AsNum.XFControls.Droid {
 
             if (e.OldElement != null) {
                 if (this.Control != null)
-                    this.Control.RemoveJavascriptInterface("native");
+                    this.Control.RemoveJavascriptInterface("CS");
             }
 
             if (e.NewElement != null) {
@@ -34,22 +37,67 @@ namespace AsNum.XFControls.Droid {
                 var ctrl = new AW.WebView(this.Context);
                 ctrl.Settings.SetGeolocationEnabled(true);
                 ctrl.Settings.JavaScriptEnabled = true;
-                ctrl.SetWebViewClient(new BaiduMapWebViewClient());
+
+                var client = new BaiduMapWebViewClient();
+                client.OnPageLoaded += Client_OnPageLoaded;
+                ctrl.SetWebViewClient(client);
                 ctrl.SetWebChromeClient(new BaiduMapWebChromeClient());
 
                 // https://developer.xamarin.com/recipes/android/controls/webview/call_csharp_from_javascript/
-                ctrl.AddJavascriptInterface(new BaiduMapJsInterface(this.Context), "native");
+                ctrl.AddJavascriptInterface(new BaiduMapJsInterface(this.Context), "CS");
                 this.SetNativeControl(ctrl);
                 this.Control.LoadUrl("file:///android_asset/BaiduMap.html");
+
+                //this.Init();
             }
         }
+
+        private void Client_OnPageLoaded(object sender, EventArgs e) {
+            this.Init();
+        }
+
+        private void Init() {
+            if (!string.IsNullOrEmpty(this.Element.AK)) {
+                this.ExecuteJs(string.Format("page.Init('{0}')", this.Element.AK));
+            }
+        }
+
+        private void ExecuteJs(string script) {
+            this.Control.LoadUrl(string.Format("javascript:{0};", script));
+        }
+
+
+
+
+
+
 
 
         public class BaiduMapWebViewClient : WebViewClient {
+
+            public event EventHandler OnPageLoaded = null;
+
             public override bool ShouldOverrideUrlLoading(AW.WebView view, string url) {
                 return base.ShouldOverrideUrlLoading(view, url);
             }
+
+            public override void OnPageFinished(AW.WebView view, string url) {
+                base.OnPageFinished(view, url);
+                if (this.OnPageLoaded != null)
+                    this.OnPageLoaded.Invoke(view, new EventArgs());
+            }
+
         }
+
+
+
+
+
+
+
+
+
+
 
         public class BaiduMapWebChromeClient : WebChromeClient {
             public override void OnGeolocationPermissionsShowPrompt(string origin, GeolocationPermissions.ICallback callback) {
@@ -57,6 +105,11 @@ namespace AsNum.XFControls.Droid {
                 callback.Invoke(origin, true, false);
             }
         }
+
+
+
+
+
 
         public class BaiduMapJsInterface : Java.Lang.Object {
 
@@ -66,6 +119,12 @@ namespace AsNum.XFControls.Droid {
                 this.Context = context;
             }
 
+            [Export]
+            [JavascriptInterface]
+            public void ShowToast(string msg) {
+                Toast.MakeText(this.Context, msg, ToastLength.Long)
+                    .Show();
+            }
         }
     }
 }
--- AsNum.XFControls/WebBaiduMap.cs
+++ AsNum.XFControls/WebBaiduMap.cs
@@ -7,5 +7,26 @@ using Xamarin.Forms;
 
 namespace AsNum.XFControls {
     public class WebBaiduMap : View {
+
+        //public static readonly BindableProperty AKProperty =
+        //    BindableProperty.Create("AK",
+        //        typeof(string),
+        //        typeof(WebBaiduMap),
+        //        "",
+        //        propertyChanged:AKChanged);
+
+        //private static void AKChanged(BindableObject bindable, object oldValue, object newValue) {
+
+        //}
+
+        public string AK {
+            get; set;
+            //get {
+            //    return (string)this.GetValue(AKProperty);
+            //}
+            //set {
+            //    this.SetValue(AKProperty, value);
+            //}
+        }
     }
 }
