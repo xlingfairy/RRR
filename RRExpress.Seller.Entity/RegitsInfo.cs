0fa06f678e4435b3be958aa092d185ca6a2c29ba
xlingfairy <gruan@asnum.com>
2016-08-11 00:17:43 +0800

    Seller Regist

---
+++ RRExpress.Seller.Entity/RegitsInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRExpress.Seller.Entity {
+    public class RegitsInfo {
+
+        public string Address { get; set; }
+
+        public string RealName { get; set; }
+
+        public string ShopName { get; set; }
+
+        public string MainBussiness { get; set; }
+
+        public string Phone { get; set; }
+
+        public IEnumerable<byte> Photos { get; set; }
+
+        public bool IsAccept { get; set; }
+    }
+}
