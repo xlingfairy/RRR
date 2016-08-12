7880a0990ec33045a27c99f6b9c47e6357e25fb6
gruan@asnum.com <gruan@asnum.com>
2016-08-12 16:27:41 +0800

    CheckBox

--- AsNum.XFControls/CheckBox.cs
+++ AsNum.XFControls/CheckBox.cs
@@ -1,4 +1,4 @@
-﻿using NControl.Abstractions;
+﻿using AsNum.XFControls.Binders;
 using NGraphics;
 using System;
 using System.Collections.Generic;
@@ -7,10 +7,11 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace AsNum.XFControls {
-    public class CheckBox : NControlView {
+    public class CheckBox : ContentView {
 
         #region IsChecked
         public static readonly BindableProperty CheckedProperty =
@@ -18,7 +19,6 @@ namespace AsNum.XFControls {
                 typeof(bool),
                 typeof(CheckBox),
                 false,
-                BindingMode.TwoWay,
                 propertyChanged: CheckedChanged
                 );
 
@@ -34,7 +34,13 @@ namespace AsNum.XFControls {
 
         private static void CheckedChanged(BindableObject bindable, object oldValue, object newValue) {
             var chk = (CheckBox)bindable;
-            chk.Invalidate();
+            //chk.Icon.Invalidate();
+            chk.Icon.Source = new StreamImageSource() {
+                Stream = cancel => {
+                    var datas = chk.Checked ? CheckedImg : UnCheckedImg;
+                    return Task.FromResult<Stream>(new MemoryStream(datas));
+                }
+            };
         }
         #endregion
 
@@ -64,7 +70,8 @@ namespace AsNum.XFControls {
                 typeof(string),
                 typeof(CheckBox),
                 null,
-                propertyChanged: LabelChanged);
+                defaultBindingMode: BindingMode.TwoWay
+                );
 
 
         public string Text {
@@ -75,64 +82,92 @@ namespace AsNum.XFControls {
                 this.SetValue(TextProperty, value);
             }
         }
-
-        private static void LabelChanged(BindableObject bindable, object oldValue, object newValue) {
-
-        }
         #endregion
 
 
-        private static readonly Graphic CheckedG;
-        private static readonly Graphic UnCheckedG;
+        //private static readonly Graphic CheckedG;
+        //private static readonly Graphic UnCheckedG;
+
+        private static readonly byte[] CheckedImg;
+        private static readonly byte[] UnCheckedImg;
 
         static CheckBox() {
-            CheckedG = GetGraphic("AsNum.XFControls.Svgs.plus.svg");
-            UnCheckedG = GetGraphic("AsNum.XFControls.Svgs.minus.svg");
+            CheckedImg = GetImgSream("AsNum.XFControls.Imgs.checkbox_empty.png");
+            UnCheckedImg = GetImgSream("AsNum.XFControls.Imgs.checkbox_full.png");
         }
 
-        private static Graphic GetGraphic(string svgFile) {
+        //private static Graphic GetGraphic(string svgFile) {
+        //    var asm = typeof(CheckBox).GetTypeInfo().Assembly;
+        //    var stmReader = new StreamReader(asm.GetManifestResourceStream(svgFile));
+        //    var svgReader = new SvgReader(stmReader);
+        //    return svgReader.Graphic;
+        //}
+
+        private static byte[] GetImgSream(string imgFile) {
             var asm = typeof(CheckBox).GetTypeInfo().Assembly;
-            var stmReader = new StreamReader(asm.GetManifestResourceStream(svgFile));
-            var svgReader = new SvgReader(stmReader);
-            return svgReader.Graphic;
+            using (var stm = asm.GetManifestResourceStream(imgFile)) {
+                return stm.GetBytes();
+            }
         }
 
         private readonly Grid Grid;
         private readonly Label Label;
+        //private readonly NControlView Icon;
+        private readonly Image Icon;
+        private ICommand TapCmd { get; }
 
         public CheckBox() {
+            this.TapCmd = new Command(() => {
+                this.Checked = !this.Checked;
+            });
+
             var cols = new ColumnDefinitionCollection();
             cols.Add(new ColumnDefinition() { Width = GridLength.Auto });
             cols.Add(new ColumnDefinition() { Width = GridLength.Auto });
             this.Grid = new Grid() {
-                ColumnDefinitions = cols
+                ColumnDefinitions = cols,
             };
+            //this.Grid.GestureRecognizers.Add(new TapGestureRecognizer() {
+            //    Command = this.TapCmd
+            //});
+            Binders.TapBinder.SetCmd(this.Grid, this.TapCmd);
 
             this.Content = this.Grid;
 
-            this.Label = new Label();
-            this.Label.SetValue(Label.TextProperty, this.Text);
-            this.Label.SetValue(Label.IsVisibleProperty, this.ShowLabel);
+            this.Label = new Label() { BindingContext = this };
+            this.Label.SetBinding(Label.TextProperty, "Text");
+            this.Label.SetBinding(Label.IsVisibleProperty, "ShowLabel");
             this.Label.SetValue(Grid.ColumnProperty, 1);
-
+            //TapBinder.SetCmd(this.Label, this.TapCmd);
             this.Grid.Children.Add(this.Label);
 
-            //this.DrawingFunction = (canvas, rect) => this.DrawSvg(canvas, rect);
+
+            //this.Icon = new NControlView() {
+            //    DrawingFunction = (canvas, rect) => this.DrawIcon(canvas, rect)
+            //};
+            this.Icon = new Image() {
+                WidthRequest = 30,
+                HeightRequest = 30,
+                Source = new StreamImageSource() {
+                    Stream = cancel => {
+                        return Task.FromResult<Stream>(new MemoryStream(UnCheckedImg));
+                    }
+                }
+            };
+            this.Icon.SetValue(Grid.ColumnProperty, 0);
+            //TapBinder.SetCmd(this.Label, this.TapCmd);
+            this.Grid.Children.Add(this.Icon);
         }
 
-        public override void Draw(ICanvas canvas, Rect rect) {
-            base.Draw(canvas, rect);
+        //private void DrawIcon(ICanvas canvas, Rect rect) {
+        //    canvas.FillRectangle(rect, new SolidBrush(NGraphics.Color.FromRGB(0, 0, 0, 0)));
+        //    //canvas.DrawText("Test", rect, new NGraphics.Font(), NGraphics.TextAlignment.Center, NGraphics.Color.FromRGB(0xff, 0, 0));
 
-            canvas.FillRectangle(rect, new SolidBrush(NGraphics.Color.FromRGB(0, 0, 0, 0)));
-
-            canvas.DrawText("Test", rect, new NGraphics.Font(), NGraphics.TextAlignment.Center, NGraphics.Color.FromRGB(0xff, 0, 0));
-
-            if (this.Checked) {
-                CheckedG.Draw(canvas);
-            }
-            else {
-                UnCheckedG.Draw(canvas);
-            }
-        }
+        //    if (this.Checked) {
+        //        CheckedG.Draw(canvas);
+        //    } else {
+        //        UnCheckedG.Draw(canvas);
+        //    }
+        //}
     }
 }
--- AsNum.XFControls/Helper.cs
+++ AsNum.XFControls/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.CSharp.RuntimeBinder;
 using System;
+using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace AsNum.XFControls {
@@ -18,8 +19,7 @@ namespace AsNum.XFControls {
         public static object TryGetProperty(object target, string name) {
             try {
                 return GetProperty(target, name);
-            }
-            catch {
+            } catch {
                 return null;
             }
         }
@@ -27,10 +27,35 @@ namespace AsNum.XFControls {
         public static T GetProperty<T>(object target, string name, T defaultValue = default(T)) {
             try {
                 return (T)GetProperty(target, name);
-            }
-            catch {
+            } catch {
                 return defaultValue;
             }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stm"></param>
+        /// <param name="perCount"></param>
+        /// <returns></returns>
+        public static byte[] GetBytes(this Stream stm, int perCount = 1024) {
+            if (stm == null)
+                throw new ArgumentNullException("stm");
+            if (perCount <= 0)
+                throw new ArgumentOutOfRangeException("perCount", "perCount 必须大于等于0");
+
+            if (stm.CanSeek)
+                stm.Position = 0;
+
+            byte[] bytes = new byte[stm.Length];
+            var offset = 0;
+            var count = 0;
+            while (0 != (count = stm.Read(bytes, offset, stm.Length - stm.Position > perCount ? perCount : (int)(stm.Length - stm.Position)))) {
+                offset += count;
+            }
+
+            return bytes;
         }
     }
 }
