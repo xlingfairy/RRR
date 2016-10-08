68565825f3d69f131e327c2928f5fd0399fe2bdf
gruan@asnum.com <gruan@asnum.com>
2016-10-08 17:50:13 +0800

    Toast for ios

--- AsNum.XFControls.iOS/Services/ToastImpl.cs
+++ AsNum.XFControls.iOS/Services/ToastImpl.cs
@@ -1,5 +1,7 @@
 ﻿using AsNum.XFControls.iOS.Services;
 using AsNum.XFControls.Services;
+using Foundation;
+using UIKit;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(ToasImpl))]
@@ -7,6 +9,19 @@ namespace AsNum.XFControls.iOS.Services {
     public class ToasImpl : IToast {
 
         public void Show(string msg, bool longShow = false) {
+            var font = UIFont.SystemFontOfSize(14F);
+
+            var lbl = new UILabel() {
+                Text = msg,
+                Font = font,
+                TextColor = UIColor.White
+            };
+
+            lbl.SizeToFit();
+
+            Toast.Instance.SetContent(lbl);
+            Toast.Instance.Show(duration: longShow ? Toast.Durations.Long : Toast.Durations.Short);
+
             //try {
             //    Device.BeginInvokeOnMainThread(() => {
             //        var toast = Toast.MakeText(Forms.Context, msg, longShow ? ToastLength.Long : ToastLength.Short);
---
+++ AsNum.XFControls.iOS/Toast.cs
@@ -0,0 +1,98 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+using Xamarin.Forms;
+
+namespace AsNum.XFControls.iOS {
+    public class Toast : NSObject {
+
+        private static Lazy<Toast> _Instance = new Lazy<Toast>(() => new Toast());
+
+
+        public static Toast Instance {
+            get {
+                return _Instance.Value;
+            }
+        }
+
+
+        private Toast() {
+
+        }
+
+
+        private Lazy<UIView> Container = new Lazy<UIView>(() => {
+            var view = new UIView();
+            view.Layer.CornerRadius = 5f;
+            view.Layer.BackgroundColor = new CoreGraphics.CGColor(0, 0, 0, 0.75f);
+            view.AutoresizingMask = UIViewAutoresizing.All;
+
+            return view;
+        });
+
+        private UIView SubView = null;
+
+        public void SetContent(UIView view) {
+            if (this.SubView != null) {
+                this.SubView.RemoveFromSuperview();
+            }
+            this.SubView = view;
+            this.Container.Value.AddSubview(view);
+
+            var window = UIApplication.SharedApplication.KeyWindow;
+
+            this.Container.Value.Frame = new CoreGraphics.CGRect(0, 0, view.Frame.Width + 10, view.Frame.Height + 10);
+            this.Container.Value.Center = new CoreGraphics.CGPoint(window.Center.X, 400);
+            view.Center = this.Container.Value.Center;
+        }
+
+        public void Show(Positions pos = Positions.Bottom, Durations duration = Durations.Short) {
+            this.Dismiss();
+
+            var window = UIApplication.SharedApplication.KeyWindow;
+            window.AddSubview(this.Container.Value);
+
+            var ms = duration == Durations.Long ? 3000 : 1000;
+            Task.Delay(ms)
+                .ContinueWith(t =>
+                    this.Dismiss()
+                );
+        }
+
+        public void Dismiss() {
+            this.Container.Value.RemoveFromSuperview();
+        }
+
+
+        #region dispose
+        private bool IsDisposed = false;
+
+        protected override void Dispose(bool disposing) {
+            if (disposing && !this.IsDisposed) {
+                this.IsDisposed = true;
+
+                if (this.Container != null && this.Container.IsValueCreated) {
+                    this.Container.Value.Dispose();
+                    this.Container = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        public enum Positions {
+            Top,
+            Center,
+            Bottom
+        }
+
+        public enum Durations {
+            Long,
+            Short
+        }
+    }
+}
